ddc6538bb4f2dac2c747bd4a4d6899d19a05c8ec
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2020-03-03 01:36:46 +0200

    Added buildDeck() method for Paladin.

--- src/model/heroes/Paladin.java
+++ src/model/heroes/Paladin.java
@@ -1,7 +1,31 @@
 package model.heroes;
 
+import model.cards.Rarity;
+import model.cards.minions.Minion;
+import model.cards.spells.LevelUp;
+import model.cards.spells.SealOfChampions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Paladin extends Hero {
     public Paladin() {
         super("Uther Lightbringer");
+    }
+
+    @Override
+    public void buildDeck() throws IOException {
+        ArrayList<Minion> allNeutralMinions = getAllNeutralMinions("neutral_minions.csv");
+        ArrayList<Minion> neutralMinions = getNeutralMinions(allNeutralMinions, 15);
+        getDeck().addAll(neutralMinions);
+        getDeck().add(new SealOfChampions());
+        getDeck().add(new SealOfChampions());
+        getDeck().add(new LevelUp());
+        getDeck().add(new LevelUp());
+        Minion minion = new Minion("TirionFordring", 4, Rarity.LEGENDARY, 6, 6, true, true,
+                false);
+        getDeck().add(minion);
+        Collections.shuffle(getDeck());
     }
 }
