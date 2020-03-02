e69b03dc3aabde0ab726d7ef484f26282031f388
Mo'men Shoman <MoamenShoman>
2020-03-02 23:34:58 +0200

    commit

--- src/model/cards/spells/KillCommand.java
+++ src/model/cards/spells/KillCommand.java
@@ -1,4 +1,9 @@
 package model.cards.spells;
 
-public class KillCommand extends Spell implements MinionTargetSpell  {
+import model.cards.Rarity;
+
+public class KillCommand extends Spell implements MinionTargetSpell, HeroTargetSpell {
+    public KillCommand(String name, int manaCost, Rarity rarity) {
+        super("Kill Command", 3, Rarity.COMMON);
+    }
 }
