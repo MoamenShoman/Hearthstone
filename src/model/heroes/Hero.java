5e82c82319f45a497c97bc34048b9d0454468f2f
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2020-03-03 00:40:51 +0200

    Added getAllNeutralMinions method.

--- src/model/heroes/Hero.java
+++ src/model/heroes/Hero.java
@@ -1,7 +1,12 @@
 package model.heroes;
 
 import model.cards.Card;
+import model.cards.Rarity;
 import model.cards.minions.Minion;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 abstract public class Hero {
@@ -70,5 +75,30 @@ abstract public class Hero {
 
     public void setField(ArrayList<Minion> field) {
         this.field = field;
+    }
+
+    private static ArrayList<Minion> getAllNeutralMinions(String filePath) throws IOException {
+        String currentLine = "";
+        ArrayList<Minion> neutralMinions = new ArrayList<>();
+        FileReader fileReader = new FileReader(filePath);
+        BufferedReader br = new BufferedReader(fileReader);
+        while ((currentLine = br.readLine()) != null) {
+            String[] result = currentLine.split(",");
+            String name = result[0];
+            int manaCost = Integer.parseInt(result[1]);
+            Rarity rarity = result[2].equals("b") ? Rarity.BASIC :
+                            result[2].equals("c") ? Rarity.COMMON :
+                            result[2].equals("r") ? Rarity.RARE :
+                            result[2].equals("e") ? Rarity.EPIC :
+                            Rarity.LEGENDARY;
+            int attack = Integer.parseInt(result[3]);
+            int maxHP = Integer.parseInt(result[4]);
+            boolean taunt = result[5].equals("TRUE");
+            boolean divine = result[6].equals("TRUE");
+            boolean charge = result[7].equals("TRUE");
+            Minion minion = new Minion(name, manaCost, rarity, attack, maxHP, taunt, divine, charge);
+            neutralMinions.add(minion);
+        }
+        return neutralMinions;
     }
 }
