ffb984d6883587bc3ea2fd65fb1e5f71a28afab7
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2020-03-03 13:45:44 +0200

    Reformatted

--- src/exceptions/TauntBypassException.java
+++ src/exceptions/TauntBypassException.java
@@ -4,7 +4,6 @@ public class TauntBypassException extends HearthstoneException {
     public TauntBypassException() {
         super("Opponent has a taunt minion(s).");
     }
-
     public TauntBypassException(String message) {
         super(message);
     }
