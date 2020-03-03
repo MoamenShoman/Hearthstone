dc63fe75de5bd9aef9e9bfa61ac47b129e1fe2b9
MohamedShetewi <MuhamedShetewi@gmail.com>
2020-03-03 02:08:32 +0200

    Added getNeutralMinions method.

--- src/model/heroes/Priest.java
+++ src/model/heroes/Priest.java
@@ -1,7 +1,36 @@
 package model.heroes;
 
+import model.cards.Rarity;
+import model.cards.minions.Minion;
+import model.cards.spells.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Priest extends Hero {
-    public Priest() {
+
+
+    public Priest() throws IOException {
         super("Anduin Wrynn");
+
+    }
+
+    public void buildDeck() throws IOException {
+        ArrayList<Minion> allNeutralMinions = getAllNeutralMinions("neutral_minions.csv");
+        ArrayList<Minion> neutralMinions = getNeutralMinions(allNeutralMinions, 13);
+        getDeck().addAll(neutralMinions);
+        getDeck().add(new DivineSpirit());
+        getDeck().add(new DivineSpirit());
+        getDeck().add(new HolyNova());
+        getDeck().add(new HolyNova());
+        getDeck().add(new ShadowWordDeath());
+        getDeck().add(new ShadowWordDeath());
+
+        Minion minion = new Minion("Prophet Velen", 7, Rarity.LEGENDARY, 7,7, false, false,
+                false);
+        getDeck().add(minion);
+        Collections.shuffle(getDeck());
+
     }
 }
--- src/model/heroes/Warlock.java
+++ src/model/heroes/Warlock.java
@@ -1,7 +1,34 @@
 package model.heroes;
 
+import model.cards.Rarity;
+import model.cards.minions.Minion;
+import model.cards.spells.CurseOfWeakness;
+import model.cards.spells.SiphonSoul;
+import model.cards.spells.TwistingNether;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Warlock extends Hero {
-    public Warlock() {
+    public Warlock() throws IOException {
         super("Gul’dan");
+    }
+
+    @Override
+    public void buildDeck() throws IOException {
+        ArrayList<Minion> allNeutralMinions = getAllNeutralMinions("neutral_minions.csv");
+        ArrayList<Minion> neutralMinions = getNeutralMinions(allNeutralMinions, 13);
+        getDeck().addAll(neutralMinions);
+        getDeck().add(new CurseOfWeakness());
+        getDeck().add(new CurseOfWeakness());
+        getDeck().add(new SiphonSoul());
+        getDeck().add(new SiphonSoul());
+        getDeck().add(new TwistingNether());
+        getDeck().add(new TwistingNether());
+        Minion minion = new Minion("Wilfred Fizzlebang", 6, Rarity.LEGENDARY, 4,4 , false, false,
+                false);
+        getDeck().add(minion);
+        Collections.shuffle(getDeck());
     }
 }
