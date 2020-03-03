3fc308e511df3ce42668a9f3c38cc0988bb6be4a
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2020-03-03 12:02:39 +0200

    modified constructor of Icehowl.

--- src/model/cards/minions/Icehowl.java
+++ src/model/cards/minions/Icehowl.java
@@ -4,7 +4,7 @@ import model.cards.Rarity;
 
 public class Icehowl extends Minion {
 
-    public Icehowl(String name, int manaCost, Rarity rarity, int attack, int maxHP, boolean taunt, boolean divine, boolean charge) {
+    public Icehowl() {
         super("Icehowl", 9, Rarity.LEGENDARY, 10, 10, false, false, true);
     }
 }
