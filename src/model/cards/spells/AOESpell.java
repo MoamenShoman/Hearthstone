fd853bb6c265abab1a121c1da968868f90df5692
Mo'men Shoman <MoamenShoman>
2020-03-19 21:57:50 +0200

    commit

--- src/model/cards/spells/AOESpell.java
+++ src/model/cards/spells/AOESpell.java
@@ -1,5 +1,8 @@
 package model.cards.spells;
+import model.cards.minions.Minion;
+import java.util.ArrayList;
 
 public interface AOESpell {
+    public void performAction(ArrayList<Minion> oppField, ArrayList<Minion> curField);
 
 }
--- src/model/cards/spells/FieldSpell.java
+++ src/model/cards/spells/FieldSpell.java
@@ -1,5 +1,8 @@
 package model.cards.spells;
+import model.cards.minions.Minion;
+import java.util.ArrayList;
 
 public interface FieldSpell {
+    public void performAction(ArrayList<Minion> field);
 
 }
