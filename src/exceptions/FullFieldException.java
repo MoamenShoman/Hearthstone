5e1cce066306e532e357ca42d8af2683d2bed0c1
Mo'men Shoman <MoamenShoman>
2020-03-19 21:54:44 +0200

    commit

--- src/exceptions/FullFieldException.java
+++ src/exceptions/FullFieldException.java
@@ -2,7 +2,7 @@ package exceptions;
 
 public class FullFieldException extends HearthstoneException {
     public FullFieldException() {
-        super("The Field is Full. You can't summon more minions");
+        super("The Field is Full. You can't summon more minions.");
     }
 
     public FullFieldException(String message) {
--- src/model/cards/Card.java
+++ src/model/cards/Card.java
@@ -3,7 +3,7 @@ package model.cards;
 
 abstract public class Card {
     private String name;
-    private int manaCost;   // 0 < manaCost <= 10
+    private int manaCost;   // 0 <= manaCost <= 10
     private Rarity rarity;
 
     public Card(String name, int manaCost, Rarity rarity) {
