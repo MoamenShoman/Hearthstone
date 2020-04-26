8df78e051ffb6eee0abc53f3295fbd5810338907
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2020-04-27 00:39:29 +0200

    commit

--- src/Controller/Client.java
+++ src/Controller/Client.java
@@ -53,7 +53,7 @@ public class Client {
     }
 
     public static void main(String args[]) {
-        Client client = new Client("127.0.0.1", 5000);
+        Client client = new Client("156.219.90.240", 6000);
     }
 }
 
