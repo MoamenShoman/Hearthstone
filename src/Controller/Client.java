0e9a657a8b0027a6ab3ab7df82202ccb600ef627
Mo'men Shoman <MoamenShoman>
2020-04-27 00:10:38 +0200

    commit

--- src/Controller/Client.java
+++ src/Controller/Client.java
@@ -53,7 +53,7 @@ public class Client {
     }
 
     public static void main(String args[]) {
-        Client client = new Client("127.0.0.1", 5000);
+        Client client = new Client("197.36.225.96", 6000);
     }
 }
 
