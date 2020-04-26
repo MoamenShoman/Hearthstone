af10995a7ec5fc3f1a3fc7234e9189e50c8bc8fd
Mo'men Shoman <MoamenShoman>
2020-04-26 23:21:20 +0200

    commit

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -20,7 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class GameView extends JFrame{
+public class GameView extends JFrame {
 
     private ArrayList<JTextArea> curFieldHPs, curFieldAttacks, curFieldManaCosts, oppFieldHPs, oppFieldAttacks, oppFieldManaCosts,
             curHandHPs, curHandAttacks, curHandManaCosts;
@@ -127,10 +127,9 @@ public class GameView extends JFrame{
         startButton0.setIcon(startImageIcon);
         startButton0.setContentAreaFilled(false);
         startButton0.setBorderPainted(false);
-        Insets insets =startButton0.getInsets();
-        Dimension size=startButton0.getPreferredSize();
-        startButton0.setBounds(insets.left+445, insets.top+150, size.width-500, size.height-390);
-
+        Insets insets = startButton0.getInsets();
+        Dimension size = startButton0.getPreferredSize();
+        startButton0.setBounds(insets.left + 445, insets.top + 150, size.width - 500, size.height - 390);
 
 
         exitButton0 = new JButton();
@@ -139,9 +138,9 @@ public class GameView extends JFrame{
         exitButton0.setIcon(exitIcon);
         exitButton0.setContentAreaFilled(false);
         exitButton0.setBorderPainted(false);
-        insets =exitButton0.getInsets();
-        size=exitButton0.getPreferredSize();
-        exitButton0.setBounds(insets.left+600, insets.top+450 ,size.width-810, size.height-390);
+        insets = exitButton0.getInsets();
+        size = exitButton0.getPreferredSize();
+        exitButton0.setBounds(insets.left + 600, insets.top + 450, size.width - 810, size.height - 390);
 
 
         add(startButton0);
@@ -149,7 +148,6 @@ public class GameView extends JFrame{
 
         //start.add(startButton0);
         //exit.add(exitButton0);
-
 
 
         revalidate();
@@ -433,7 +431,7 @@ public class GameView extends JFrame{
         curentPanel.add(endTurnButton);
         size = endTurnButton.getPreferredSize();
         endTurnButton.setContentAreaFilled(false);
-        endTurnButton.setBounds(insets.left + 1291, insets.top, size.width-248, size.height-148);
+        endTurnButton.setBounds(insets.left + 1291, insets.top, size.width - 248, size.height - 148);
 
         heroPowerButton = new JButton();
         Image heroPowerImage = ImageIO.read(new File("HeroPower.png"));
@@ -446,7 +444,7 @@ public class GameView extends JFrame{
         heroPowerButton.setContentAreaFilled(false);
         size = heroPowerButton.getPreferredSize();
         insets = heroPowerButton.getInsets();
-        heroPowerButton.setBounds(insets.left+1255 , insets.top+45 , size.width-185, size.height-100);
+        heroPowerButton.setBounds(insets.left + 1255, insets.top + 45, size.width - 185, size.height - 100);
 
         add(oppPanel);
         add(curentPanel);
@@ -699,7 +697,7 @@ public class GameView extends JFrame{
         Dimension size;
         Insets insets = curentPanel.getInsets();
         size = curHandLeft.getPreferredSize();
-        curHandLeft.setBounds(insets.left, insets.top + 190, size.width + 142*5, size.height +196);
+        curHandLeft.setBounds(insets.left, insets.top + 190, size.width + 142 * 5, size.height + 196);
         for (int i = 0; i < 5 && j < cards.size(); i++, j++) {
             JButton b = new JButton();
             String p;
@@ -724,7 +722,7 @@ public class GameView extends JFrame{
             curHand.add(b);
         }
         int rem = 5 - curHand.size();
-        while(rem > 0){
+        while (rem > 0) {
             JButton b = new JButton();
             b.setPreferredSize(new Dimension(140, 193));
             b.setContentAreaFilled(false);
@@ -760,7 +758,7 @@ public class GameView extends JFrame{
             curHand.add(b);
         }
         rem = 10 - curHand.size();
-        while(rem > 0){
+        while (rem > 0) {
             JButton b = new JButton();
             b.setPreferredSize(new Dimension(140, 193));
             b.setContentAreaFilled(false);
@@ -898,3 +896,4 @@ public class GameView extends JFrame{
 
     }
 }
+
