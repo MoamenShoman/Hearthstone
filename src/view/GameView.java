ad827b983843bb7fef90fb125e4af623606bafff
Mo'men Shoman <MoamenShoman>
2020-04-27 02:17:43 +0200

    commit

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -431,11 +431,11 @@ public class GameView extends JFrame {
         curentPanel.add(endTurnButton);
         size = endTurnButton.getPreferredSize();
         endTurnButton.setContentAreaFilled(false);
-        endTurnButton.setBounds(insets.left + 1291, insets.top, size.width - 248, size.height - 148);
+        endTurnButton.setBounds(insets.left + 1320, insets.top, size.width - 248, size.height - 148);
 
         heroPowerButton = new JButton();
         Image heroPowerImage = ImageIO.read(new File("HeroPower.png"));
-        ImageIcon heroPowerImageIcon = new ImageIcon(heroPowerImage.getScaledInstance(300, -1, Image.SCALE_SMOOTH));
+        ImageIcon heroPowerImageIcon = new ImageIcon(heroPowerImage);
         heroPowerButton.setIcon(heroPowerImageIcon);
         heroPowerButton.setContentAreaFilled(false);
         heroPowerButton.setBorderPainted(false);
@@ -444,7 +444,7 @@ public class GameView extends JFrame {
         heroPowerButton.setContentAreaFilled(false);
         size = heroPowerButton.getPreferredSize();
         insets = heroPowerButton.getInsets();
-        heroPowerButton.setBounds(insets.left + 1255, insets.top + 45, size.width - 185, size.height - 100);
+        heroPowerButton.setBounds(insets.left + 1300, insets.top + 55, size.width - 243, size.height - 143);
 
         add(oppPanel);
         add(curentPanel);
